4b5481c4f7302eb0389c4967e62dc5d25c575b82
EvanM1995 <evanmorgan1995@gmail.com>
2023-04-24 20:19:28 -0500

    added code to the word counter

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("In Here");
+console.log("Connected!")
+
+const wordCounter = (value) => {
+  console.log(value);
+  if (value) {
+    wordCount.innerHTML = `Word Count: 0`;
+  
+  } else { 
+
+    error.innerHTML = "";
+  }
+}
+
+const textarea = document.querySelector("textarea");
+const form = document.querySelector("form");
+const error = document.querySelector("#error");
+const wordCount = document.querySelector("#word-count");
+const toggleButton = document.querySelector("#bg-switch");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  error.innerHTML = "";
+  wordCount.innerHTML = "";
+  const value = textarea.value;
+  wordCounter(value);
+});
+
+form.addEventListener("reset", () => {
+  error.innerHTML = "";
+  wordCount.innerHTML = "";
+});
+
+toggleButton.addEventListener("click", (event) => {
+  toggleMode(event.target.innerHTML);
+});
